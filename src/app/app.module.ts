f85591c3563b1896a3f71bad559cda9becb66f6f
lucquach23 <hoangluc1002@gmail.com>
2020-09-24 22:12:19 +0700

    update 24/09

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { Routes,RouterModule } from "@angular/router";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { GiangvienComponent } from './main-content/giangvien/giangvien.component';
+import { SinhvienComponent } from './main-content/sinhvien/sinhvien.component';
+import {LoginComponent} from './login/login.component'
+const route:Routes=[
+  {
+    path: '',
+    component: SinhvienComponent
+  },
+    {
+      path: 'sinhvien',
+      component: SinhvienComponent
+    },
+    {
+      path: 'giangvien',
+      component: GiangvienComponent
+    },
+    {
+      path:'login',
+      component:LoginComponent
+    }
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SlidebarComponent,
+    MainContentComponent,
+    GiangvienComponent,
+    SinhvienComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(route)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main-content/giangvien/giangvien.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-giangvien',
+  templateUrl: './giangvien.component.html',
+  styleUrls: ['./giangvien.component.css']
+})
+export class GiangvienComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('Da vao giang vien');
+    
+  }
+
+}
