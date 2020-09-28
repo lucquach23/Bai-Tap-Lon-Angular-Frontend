5997a6442c033910752576ed07babb5770c02c7f
lucquach23 <hoangluc1002@gmail.com>
2020-09-28 21:21:22 +0700

    update 28/09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Routes,RouterModule } from "@angular/router";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SlidebarComponent } from './slidebar/slidebar.component';
-import { MainContentComponent } from './main-content/main-content.component';
-import { GiangvienComponent } from './main-content/giangvien/giangvien.component';
-import { SinhvienComponent } from './main-content/sinhvien/sinhvien.component';
-import {LoginComponent} from './login/login.component';
-import { PageStudentRegisterComponent } from './page-student-register/page-student-register.component';
-import { AdminComponent } from './admin/admin.component';
-import { ClientComponent } from './client/client.component'
-const route:Routes=[
-  {
-    path: '',
-    component: SinhvienComponent
-  },
-    {
-      path: 'sinhvien',
-      component: SinhvienComponent
-    },
-    {
-      path: 'giangvien',
-      component: GiangvienComponent
-    },
-    {
-      path:'login',
-      component:LoginComponent
-    }
-]
+import { LoginComponent } from './login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SlidebarComponent,
-    MainContentComponent,
-    GiangvienComponent,
-    SinhvienComponent,
-    PageStudentRegisterComponent,
-    AdminComponent,
-    ClientComponent
+    LoginComponent  
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(route)
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
