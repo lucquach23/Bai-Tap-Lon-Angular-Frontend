58430ec80e141fcc9c4b4e6444b27a721d270d50
lucquach23 <hoangluc1002@gmail.com>
2020-10-07 00:44:45 +0700

    update

--- src/app/main/giangvien/giangvien/giangvien.component.ts
+++ src/app/main/giangvien/giangvien/giangvien.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-giangvien',
   templateUrl: './giangvien.component.html',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GiangvienComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http:HttpClient) { }
+  values:any;
   ngOnInit(): void {
+    this.getListFaculty();
   }
+  getListFaculty(){
+    return this.http.get("https://localhost:44351/api/Faculties").subscribe(response=>{
+      console.log(response);
+      this.values=response;
+    },error =>{
+      console.log(error);
+    });
+    
+  }
+
+
+
 
 }
--- src/app/main/user/user/user.component.ts
+++ src/app/main/user/user/user.component.ts
@@ -9,82 +9,33 @@ declare var $: any;
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent extends BaseComponent implements OnInit {
-  public nguoidungs: any;
-  public uploadedFiles: any[] = [];
-  public formdata: any;
-  submitted = false;
-  @ViewChild(FileUpload, { static: false }) file_image: FileUpload;
-  constructor(private fb: FormBuilder) {
-    super();
-  }
+export class UserComponent implements OnInit {
 
-  ngOnInit(): void {
-    this.formdata = this.fb.group({
-      hoten: ['', Validators.required],
-      ngaysinh: [this.today, Validators.required],
-      diachi: [''],
-      gioitinh: ['', Validators.required],
-      email: ['', [Validators.required,Validators.email]],
-      taikhoan: ['', Validators.required],
-      matkhau: ['', [this.pwdCheckValidator]],
-      nhaplaimatkhau: ['', Validators.required],
-    }, {
-      validator: MustMatch('matkhau', 'nhaplaimatkhau')
-    });
+  constructor() {}
+  ngOnInit() {}
+  nguoidungs:any = [
+    {
+      tenhocphan: 'Tư tưởng HCM',
+      tenGV: 'Nguyễn Hữu Đông',
+      soTC: 2,
+      phuongthuc: 'Offline',
+      time: 'Sáng thứ 2,5',
+      tuanhoc:'34567',
+      phonghoc:'ĐH305',
+      trangthai:'not enough',
+      sosv: 34
+    },
+    {
+      tenhocphan: 'Toán rời rạc',
+      tenGV: 'Nguyễn Hữu Đông',
+      soTC: 2,
+      phuongthuc: 'Offline',
+      time: 'Sáng thứ 2,5',
+      tuanhoc:'34567',
+      phonghoc:'ĐH305',
+      trangthai:'not enough',
+      sosv:24
+    },
+  ];
 
-    this.nguoidungs = [
-      {
-        tenhocphan: 'Tư tưởng HCM',
-        tenGV: 'Nguyễn Hữu Đông',
-        soTC: 2,
-        phuongthuc: 'Offline',
-        time: 'Sáng thứ 2,5',
-        tuanhoc:'34567',
-        phonghoc:'ĐH305',
-        trangthai:'not enough',
-        sosv: 34
-      },
-      {
-        tenhocphan: 'Toán rời rạc',
-        tenGV: 'Nguyễn Hữu Đông',
-        soTC: 2,
-        phuongthuc: 'Offline',
-        time: 'Sáng thứ 2,5',
-        tuanhoc:'34567',
-        phonghoc:'ĐH305',
-        trangthai:'not enough',
-        sosv:24
-      },
-    ];
-  }
-
-  pwdCheckValidator(control){
-    var filteredStrings = {search:control.value, select:'@#!$%&*'}
-    var result = (filteredStrings.select.match(new RegExp('[' + filteredStrings.search + ']', 'g')) || []).join('');
-    if(control.value.length < 6 || !result){
-        return {matkhau: true};
-    }
-  }
-
-  get f() { return this.formdata.controls; }
-
-  onSubmit(value) {
-    this.submitted = true;
-    let xx = value;
-    debugger;
-    this.getEncodeFromImage(this.file_image).subscribe((data: any): void => {
-      let data_image = data == '' ? null : data;
-      //this.closeModal();
-    });
-  }
-
-  createModal() {
-    setTimeout(() => {
-      $('#createUserModal').modal('toggle');
-    });
-  }
-  closeModal() {
-    $('#createUserModal').closest('.modal').modal('hide');
-  }
 }
